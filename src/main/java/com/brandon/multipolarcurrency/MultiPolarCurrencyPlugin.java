1a7c1b011086bcda2900fd182efe24d7b851e0a0
Brandon J M Hilliard <178799182+Tenn1987@users.noreply.github.com>
2025-12-23 19:48:57 -0600

    Fixed balance constructor

--- src/main/java/com/brandon/multipolarcurrency/MultiPolarCurrencyPlugin.java
+++ src/main/java/com/brandon/multipolarcurrency/MultiPolarCurrencyPlugin.java
@@ -1,9 +1,11 @@
 package com.brandon.multipolarcurrency;
 
+import com.brandon.multipolarcurrency.commands.BalanceCommand;
 import com.brandon.multipolarcurrency.economy.Currency;
 import com.brandon.multipolarcurrency.commands.CurrencyCommand;
 import com.brandon.multipolarcurrency.economy.BackingType;
 import com.brandon.multipolarcurrency.economy.CurrencyManager;
+import com.brandon.multipolarcurrency.economy.WalletService;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Optional;
@@ -30,13 +32,14 @@ public class MultiPolarCurrencyPlugin extends JavaPlugin {
         // Only adds defaults if file is empty/missing
         this.currencyManager.bootstrapDefaultsIfEmpty();
 
+         currencyManager = new CurrencyManager(this);
+
+        WalletService walletService = new WalletService(); // or however you construct yours
+
         getCommand("currency").setExecutor(new CurrencyCommand(currencyManager));
-
-        getLogger().info("MultiPolarCurrency enabled.");
-
-
-        getCommand("currency").setExecutor(new CurrencyCommand(currencyManager));
+        getCommand("balance").setExecutor(new BalanceCommand(currencyManager, walletService));
 
         getLogger().info("MultiPolarCurrency enabled.");
     }
+
 }
